49d652e96852c13d0c4db1f530bb3650718db5fa
Brandon Artigue <bartigue87@yahoo.com>
2022-04-15 01:45:40 -0600

    styling changes

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import projects from "./projectsData";
 import Projects from "./Projects";
 
 export default function Portfolio() {
+  const [isActive, setIsActive] = useState(false);
+
+  function handleClick() {
+    setIsActive((prevState) => !prevState);
+    console.log(isActive);
+  }
   const project = projects.map((project) => {
-    return <Projects key={project.title} {...project} />;
+    return (
+      <Projects
+        key={project.title}
+        {...project}
+        isActive={isActive}
+        handleClick={handleClick}
+      />
+    );
   });
   return (
-    <>
-      <h2>Projects</h2>
+    <div class="portfolio">
+      <h2 className="about-header project-heading">Projects</h2>
       <section className="projects-section">{project}</section>
-    </>
+    </div>
   );
 }
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,13 +2,17 @@ import React from "react";
 
 export default function Projects(props) {
   return (
-    <div className="project-card">
+    <div className="project-card" onClick={props.handleClick}>
       {props.image}
-      <div>
-        <h4>{props.title}</h4>
-        <h6>{props.languages}</h6>
-        <p>{props.description}</p>
-      </div>
+      {props.isActive ? (
+        <div className="project-card-info fadein">
+          <h4 className="card-title">{props.title}</h4>
+          <h6 className="card-language">{props.languages}</h6>
+          <p className="card-description">{props.description}</p>
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
